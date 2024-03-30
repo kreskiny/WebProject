6844b4e2f94c1238826d21a1b09c874cad3a5170
eva <zykovaeva310306@gmail.com>
2024-03-30 16:02:04 +0400

    1

--- main.py
+++ main.py
@@ -8,9 +8,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/info')
-def info():
-    return render_template('info.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
