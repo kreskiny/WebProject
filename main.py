1bff11bab4f3aae7da3333235c10ea7f4a38df67
daria <d4riyabel@yandex.ru>
2024-04-03 21:46:06 +0400

    регистрация и вход

--- main.py
+++ main.py
@@ -9,5 +9,15 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/log_in')
+def log_in():
+    return render_template('log_in.html')
+
+
+@app.route('/registration')
+def registration():
+    return render_template('registration.html')
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
