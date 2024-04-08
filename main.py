343c0703a17747ab0a00619c8d53db99775da44a
eva <zykovaeva310306@gmail.com>
2024-04-08 23:03:59 +0400

    products

--- main.py
+++ main.py
@@ -22,6 +22,12 @@ def registration():
 def product():
     return render_template('product.html')
 
+@app.route('/product1')
+def product1():
+    return render_template('product1.html')
+@app.route('/product2')
+def product2():
+    return render_template('product2.html')
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
