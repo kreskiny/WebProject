5c8c04c74c8dac31570f2c1cc02db68ff02802b7
eva <zykovaeva310306@gmail.com>
2024-04-08 22:21:59 +0400

    product info

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def log_in():
 def registration():
     return render_template('registration.html')
 
+@app.route('/product')
+def product():
+    return render_template('product.html')
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
